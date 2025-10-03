30e65a25e04759856f9e9301aaea72c49977578d
alectrona6400 <78973009+alectrona6400@users.noreply.github.com>
2025-10-03 16:58:57 -0400

    Update config.m

--- config.m
+++ config.m
@@ -62,4 +62,4 @@ XFAT=32
 # Give extra compiler DEFINE flags here
 # such as -DDEBUG : extra DEBUG output
 #         -DDOSEMU : printf output goes to dosemu log
-# set ALLCFLAGS=-DDEBUG -DWIN31SUPPORT
+set ALLCFLAGS=-DDEBUG -DWIN31SUPPORT
